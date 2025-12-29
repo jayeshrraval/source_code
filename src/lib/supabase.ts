37f831709b6b96e3f271643572e66fe440aca5ef
Jayesh Raval <jayeshrraval2@gmail.com>
2025-12-29 22:46:09 +0530

    Enabled Realtime support in Supabase Client

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://ukwqvttvgguatatrzgzm.supabase.co'; 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrd3F2dHR2Z2d1YXRhdHJ6Z3ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxMjk5ODUsImV4cCI6MjA4MTcwNTk4NX0.nCcUxMYjM25e36W7h2rDcxlriIQE0ZzlOi879rVCqNI';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// ✅ Realtime options સાથે ક્લાયન્ટ બનાવો
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  realtime: {
+    params: {
+      eventsPerSecond: 10,
+    },
+  },
+});
